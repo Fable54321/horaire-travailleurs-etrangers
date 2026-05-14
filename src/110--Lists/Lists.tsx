c34d1b29952a5655a80181f3233a204bb26f17cb
fable54321 <timothe5@hotmail.com>
2026-05-14 10:22:18 -0400

    first commit

--- src/110--Lists/Lists.tsx
+++ src/110--Lists/Lists.tsx
@@ -1,22 +1,40 @@
+import { useEffect } from 'react'
 import { useWorkerSchedule } from '../Contexts/WorkerScheduleContext'
 import { formatDate } from '../Utils/formatDate'
+import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react'
 
 const Lists = () => {
-  const { selectedDate } = useWorkerSchedule()
+  const { selectedDate, setSelectedDate } = useWorkerSchedule()
+
+  const handleSetNextDay = () => {
+    const nextDate = new Date(selectedDate)
+    nextDate.setDate(nextDate.getDate() + 1)
+    setSelectedDate(nextDate)
+  }
+
+  const handleSetPreviousDay = () => {
+    const previousDate = new Date(selectedDate)
+    previousDate.setDate(previousDate.getDate() - 1)
+    setSelectedDate(previousDate)
+  }
+
+
+  useEffect(() => {
+    console.log(selectedDate)
+  },[selectedDate])
 
   return (
-    <article className="flex flex-col items-center mt-5">
-        <section className="flex flex-col items-center gap-5">
-
-          <h2>{formatDate(selectedDate)}</h2>
-      <h2 className="font-bold font-tertiary text-[1.2em]">Liste des camionneurs disponibles:</h2>
-      <ul className="flex flex-col"></ul>
-      </section>
-      <section className="flex flex-col items-center">
-        <h2 className="font-bold font-tertiary text-[1.2em]">Liste des travailleurs en congé:</h2>
-        <ul className="flex flex-col"></ul>
-      </section>
-
+    <article className="flex flex-col items-center mt-5 text-[0.9rem] md:text-[1rem] lg:text-[1.1rem]">
+       
+       <div className="flex items-center gap-3">
+<button className="text-secondary hover:cursor-pointer"  onClick={handleSetPreviousDay}><ChevronLeftIcon size={30} strokeWidth={3} /></button>
+          <h2 className="font-tertiary text-[2em] font-bold">{formatDate(selectedDate)}</h2>
+   <button className="text-secondary hover:cursor-pointer" onClick={handleSetNextDay}>
+    <ChevronRightIcon size={30} strokeWidth={3} />
+    </button> 
+  </div>
+   
+  
     </article>
   )
 }
