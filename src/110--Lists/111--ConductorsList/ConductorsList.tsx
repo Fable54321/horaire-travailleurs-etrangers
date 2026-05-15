10edd91e1e818db1c3bb9ba1c857ecf0d2133bf9
fable54321 <timothe5@hotmail.com>
2026-05-15 07:52:35 -0400

    first commit

--- src/110--Lists/111--ConductorsList/ConductorsList.tsx
+++ src/110--Lists/111--ConductorsList/ConductorsList.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useMemo } from "react"
 import { useWorkerSchedule } from "../../Contexts/WorkerScheduleContext"
-
+import wheel from "../../assets/images/wheel.png"
+import { UserRound } from "lucide-react"
 
 const ConductorsList = () => {
 
-const {selectedDate, setSelectedDate, workersSchedule} = useWorkerSchedule()
+const {selectedDate, workersSchedule} = useWorkerSchedule()
 
 useEffect(() => {
     console.log(selectedDate)
@@ -20,13 +21,20 @@ useEffect(() => {
 },[filteredWorkersSchedule])
 
   return (
-    <section className="mt-3 flex flex-col items-center bg-white p-4 rounded-xl shadow-2xl">
-        <div className="bg-[#f4f6ee] text-secondary font-tertiary px-1">
-      <h2 className="text-[1.7em] ">Lista de conductores disponibles</h2>
+    <section className="mt-3 flex flex-col items-center gap-4 bg-white p-4 rounded-xl shadow-2xl">
+        <div className="bg-[#f4f6ee]  text-secondary font-tertiary px-2 rounded-lg flex items-center gap-2 shadow-lg">
+      <h2 className="text-[1.7em] text-center">Lista de conductores disponibles</h2>
+      <div className="bg-secondary p-2 rounded-xl">
+      <img src={wheel} alt="wheel" className="w-10" />
+      </div>
+      
       </div>
       <ul>
         {filteredWorkersSchedule.map(worker => (
-          <li key={worker.id}>
+          <li key={worker.id} className="flex items-center gap-2">
+            <div className="bg-secondary p-2 rounded-md">
+            <UserRound className="text-white " />
+            </div>
            {worker.user_surname} {worker.user_name} 
           </li>
         ))}
